e83c401e5d2928cac10a15f4506ae1d87b302ca9
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-01-30 18:17:57 -0700

    Added module constants

---
+++ src/module/constants/index.ts
@@ -0,0 +1,25 @@
+/**
+ * String identifier for the module used throughout other scripts.
+ */
+export const MODULE_ID = 'cosmere-level-wizard';
+
+/**
+ * Full title string of the module.
+ */
+export const MODULE_NAME = 'Cosmere Level Wizard';
+
+/**
+ * String identifier for the system used throughout other scripts.
+ */
+export const SYSTEM_ID = 'cosmere-rpg';
+
+/**
+ * String identifiers for official modules used throughout other scripts.
+ */
+export const STORMLIGHT_HANDBOOK = 'cosmere-rpg-stormlight-handbook';
+export const STORMLIGHT_WORLDGUIDE = 'cosmere-rpg-stormlight-world-guide';
+
+/**
+ * Author name of the module.
+ */
+export const AUTHOR_NAME = 'posadist-revolution';
